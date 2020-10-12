14b5575dd977ccb9fe3af592b8a33a05cac71755
ofentsesambo <ofentsesambo@gmail.com>
2020-10-12 13:41:12 +0200

    fixed spec file

--- spec/password_is_ok_spec.js
+++ spec/password_is_ok_spec.js
@@ -1,28 +1,19 @@
 let password = require("../src/password_checker.js");
-let testPassword = "Gd1$1fta4"
-let okay = password.passwordIsOkay(testPassword);
 
 describe("password is okay ", function(){
          it("It checks to see if the password is not empty", function(){
-            expect(okay).toBe(true);
+            expect(password.passwordIsOkay('')).toBe(false);
          })
          it("It checks if the size of the password is greater than 8",function(){
-            expect(okay).toBe(true);
+            expect(password.passwordIsOkay('abdh')).toBe(false);
          })
          
          it("It checks if the password has an upper case letter", function(){
-             expect(okay).toBe(true);
+             expect(password.passwordIsOkay('ahahahahahhah')).toBe(false);
          })
 
          it("It checks if the password has a lower case letter", function(){
-             expect(okay).toBe(true);
+             expect(password.passwordIsOkay('Al@akddnd2f')).toBe(true);
          })
-
-         it("It checks if the password has at least one digit", function(){
-             expect(okay).toBe(true);
-         })
-
-         it("it checks if the password has a special character", function(){
-             expect(okay).toBe(true);
-         }) 
  })
+
